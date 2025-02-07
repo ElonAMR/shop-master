edb932f79e7ea31a840575d1b46a94ec58f2ca29
אלון עמרם <elonamrammm1234@gmail.com>
2025-02-07 15:26:45 +0200

    create Page For Arr Of Products and import in App Page Main

---
+++ src/Arr_Products.jsx
@@ -0,0 +1,53 @@
+
+const Arr_Products = [
+    {
+        id: 1,
+        image: "",
+        description: "Iphone 16 Pro 256GB",
+        price: 699.99
+    },
+    {
+        id: 2,
+        image: "",
+        description: "Redmi Note 14 256GB",
+        price: 1299.99
+    },
+    {
+        id: 3,
+        image: "",
+        description:  "Galaxy S25 Ultra 256GB",
+        price: 149.99
+    },
+    {
+        id: 4,
+        image: "",
+        description: "Galaxy Tab A9 64GB",
+        price: 199.99
+    },
+    {
+        id: 5,
+        image: "",
+        description: "Ipad Air 4 128GB",
+        price: 299.99
+    },
+    {
+        id: 6,
+        image: "",
+        description: "Galaxy Buds 3",
+        price: 89.99
+    },
+    {
+        id: 7,
+        image: "",
+        description: "AirPods 4",
+        price: 89.99
+    },
+    {
+        id: 8,
+        image: "",
+        description: "Apple Watch Se LTE 44mm",
+        price: 89.99
+    }
+];
+
+export default Arr_Products;
